8c60f685dd4c2194465b3d045966f16777a2f7d5
Mesi Rendon <amrendonsa@unal.edu.co>
2020-08-12 07:34:10 -0500

    Finishing

--- contracts/PiggyBank.sol
+++ contracts/PiggyBank.sol
@@ -2,6 +2,50 @@
 pragma solidity >= 0.5.0 < 0.7.0;
 
 contract PiggyBank {
-  constructor() public {
+  string public name;
+  address payable owner;
+  mapping(address => Saving) savings;
+
+  struct Saving {
+    uint totalAmount;
+    uint times;
+  }
+
+  constructor(string memory _name) public {
+    name = _name;
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, 'You are not the rightful owner');
+    _;
+  }
+
+  function deposit() public payable {
+    Saving storage saving = savings[msg.sender];
+    saving.totalAmount += msg.value;
+    saving.times += 1;
+  }
+
+  function withdraw() public onlyOwner {
+    owner.transfer(address(this).balance);
+  }
+
+  function depositsByAddress(address _sender) public view returns(uint totalAmount, uint times) {
+    Saving storage saving = savings[_sender];
+    totalAmount = saving.totalAmount;
+    times = saving.times;
+  }
+
+  function setOwner(address payable _newOwner) public onlyOwner {
+    owner = _newOwner;
+  }
+
+  function crash() public onlyOwner {
+    selfdestruct(owner);
+  }
+
+  function() external payable {
+    if(msg.value == 0) withdraw();
   }
 }
